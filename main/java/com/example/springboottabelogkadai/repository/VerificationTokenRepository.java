9f118f6a85bcc4ecf5d4372c316404fdcf782ce5
k-shota0827 <150798076+k-shota0827@users.noreply.github.com>
2024-03-28 23:26:21 +0900

    Delete main directory

--- main/java/com/example/springboottabelogkadai/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboottabelogkadai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springboottabelogkadai.entity.VerificationToken;
-
-public interface VerificationTokenRepository extends JpaRepository< VerificationToken, Integer> {
-	public VerificationToken findByToken(String token);
-}
--- main/resources/static/js/flatpickr.js
+++
@@ -1,9 +0,0 @@
-let maxDate = new Date();
-maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
-
-flatpickr('#fromReservationDate', {
-  mode: "single",
-  locale: 'ja',
-  minDate: 'today',
-  maxDate: maxDate
-});
